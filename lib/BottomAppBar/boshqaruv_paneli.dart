476eac92ba229a9e25c2ddd2d956b32e8f7aa38e
Izzatillocodermobile <gulomovizzatillo19@gmail.com>
2026-04-16 16:57:35 +0500

    dashboard has been updated

--- lib/BottomAppBar/boshqaruv_paneli.dart
+++ lib/BottomAppBar/boshqaruv_paneli.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:biznes_ui/BOSHQARUV_PANELI/all_active_page.dart';
 import 'package:biznes_ui/BOSHQARUV_PANELI/delete_page.dart';
@@ -45,7 +46,7 @@ class _BoshqaruvPaneliState extends State<BoshqaruvPaneli> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
+      backgroundColor: const Color.fromARGB(255, 246, 242, 242),
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text(
@@ -67,25 +68,42 @@ class _BoshqaruvPaneliState extends State<BoshqaruvPaneli> {
 
       body: Column(
         children: [
-          /// ✅ YANGI BALANCE CARD
           BalanceCard(
             balance: balance,
             onAdd: () => showmodelBottomSheet(context),
             onRemove: removeMoney,
           ),
 
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blue),
-              borderRadius: BorderRadius.circular(12),
+          Padding(
+            padding: const EdgeInsetsGeometry.only(left: 16, right: 16),
+            child: DottedBorder(
+              color: Colors.blue,
+              strokeWidth: 2,
+              dashPattern: [6, 3],
+              borderType: BorderType.RRect,
+              radius: Radius.circular(12),
+
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 12),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Center(
+                  child: Text(
+                    "+ Yangi maqsad qo'shish",
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
             ),
-            child: const Center(child: Text("+ Yangi maqsad qo'shish")),
           ),
 
           const SizedBox(height: 10),
-
           Expanded(
             child: Container(
               margin: const EdgeInsets.all(16),
@@ -153,7 +171,6 @@ class _BoshqaruvPaneliState extends State<BoshqaruvPaneli> {
   }
 }
 
-/// 🔥 BALANCE CARD (ICHIGA QO‘SHILDI)
 class BalanceCard extends StatelessWidget {
   final double balance;
   final VoidCallback onAdd;
@@ -181,12 +198,14 @@ class BalanceCard extends StatelessWidget {
             ),
             borderRadius: BorderRadius.circular(20),
           ),
-
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 10),
-
+              const SizedBox(height: 6),
+              Text(
+                " JORIY BALANS ",
+                style: TextStyle(color: Colors.white70, fontSize: 12),
+              ),
               Text(
                 "${balance.toStringAsFixed(0)} so'm",
                 style: const TextStyle(
@@ -195,102 +214,103 @@ class BalanceCard extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
-                height: 126,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 180, 224, 245),
-                  borderRadius: BorderRadius.circular(12.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      
-                      Row(
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                  child: Container(
+                    height: 126,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          const Text(
-                            " TEJASH MAQSADI ",
-                            style: TextStyle(
-                              color: Colors.white70,
-                              fontSize: 12,
+                          Row(
+                            children: [
+                              const Text(
+                                " TEJASH MAQSADI ",
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Spacer(),
+                              _iconBtn(Icons.edit, () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const Edit_page(),
+                                  ),
+                                );
+                              }),
+                              const SizedBox(width: 8),
+                              _iconBtn(Icons.delete, () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (_) => const DeletePage(),
+                                  ),
+                                );
+                              }),
+                            ],
+                          ),
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: const Text(
+                              " Yangi O'yin Konsoli  ",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
                           ),
-                          Spacer(),
-                          _iconBtn(Icons.edit, () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => const Edit_page(),
+                          SizedBox(height: 12),
+                          Stack(
+                            children: [
+                              Container(
+                                height: 6,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey.shade300,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
-                            );
-                          }),
-
-                          const SizedBox(width: 8),
-
-                          _iconBtn(Icons.delete, () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => const DeletePage(),
+                              Container(
+                                height: 6,
+                                width: 20,
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [Colors.greenAccent, Colors.green],
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.centerRight,
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
-                            );
-                          }),
-                        ],
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: const Text(
-                          textAlign: TextAlign.start,
-                          " Yangi O'yin Konsoli  ",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                            ],
                           ),
-                        ),
-                      ),
-                      SizedBox(height: 12),
-                      Stack(
-                        children: [
-                          Container(
-                            height: 6,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade300,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-
-                          Container(
-                            height: 6,
-                            width: 20,
-                            decoration: BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: [Colors.greenAccent, Colors.green],
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
+                          SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "390,500 / 10,000,000 so'm",
+                                style: TextStyle(color: Colors.white70),
                               ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
+                              Text(
+                                "360 kun qoldi",
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                            ],
                           ),
                         ],
                       ),
-                      SizedBox(height: 8),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "390,500 / 10,000,000 so'm",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                          Text(
-                            "360 kun qoldi",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
@@ -298,22 +318,37 @@ class BalanceCard extends StatelessWidget {
               const SizedBox(height: 20),
 
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(
+                  SizedBox(
+                    height: 40,
+                    width: 180,
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        minimumSize: Size(152, 40),
-                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        padding: const EdgeInsets.symmetric(vertical: 6),
                         backgroundColor: Colors.green,
                         foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
                       onPressed: onAdd,
-                      child: const Text("+ Pul qo'shish"),
+                      child: const Text("+ Pul qoshish"),
                     ),
                   ),
                   const SizedBox(width: 10),
-                  Expanded(
+                  SizedBox(
+                    height: 40,
+                    width: 180,
                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 6),
+                        backgroundColor: Colors.deepOrange,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
                       onPressed: onRemove,
                       child: const Text("- Pul olish"),
                     ),
@@ -333,7 +368,7 @@ class BalanceCard extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: Container(
-          color: Colors.white.withOpacity(0.2),
+          color: Colors.white.withOpacity(0.1),
           child: IconButton(
             icon: Icon(icon, color: Colors.white),
             onPressed: () => onTap(),
